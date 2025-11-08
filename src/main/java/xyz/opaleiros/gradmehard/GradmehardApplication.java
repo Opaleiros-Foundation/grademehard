273df06d9de9a87c2c87972e56bcca6d35a33cf5
MatheusVict <Matheus.Henrique0724@jala.universitY>
2025-11-08 16:04:04 -0300

    initial commit

---
+++ src/main/java/xyz/opaleiros/gradmehard/GradmehardApplication.java
@@ -0,0 +1,13 @@
+package xyz.opaleiros.gradmehard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradmehardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradmehardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/opaleiros/gradmehard/GradmehardApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.opaleiros.gradmehard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradmehardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
