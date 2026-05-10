aad01a247013a0a45e7c5c4ee2deb2c8c3cf7897
Mariah Eduarda <mariah.bocoli@gmail.com>
2026-05-10 12:02:14 -0300

    Add CreateUserUseCase for user registration
    Implemented CreateUserUseCase to handle user creation.

--- src/main/java/xyz/opaleiros/gradmehard/application/usecase/CreateUserUseCase.java
+++ src/main/java/xyz/opaleiros/gradmehard/application/usecase/CreateUserUseCase.java
@@ -1,4 +1,32 @@
 package xyz.opaleiros.gradmehard.application.usecase;
 
+import org.springframework.stereotype.Service;
+import xyz.opaleiros.gradmehard.application.dto.CreateUserDTO;
+import xyz.opaleiros.gradmehard.domain.entity.User;
+import xyz.opaleiros.gradmehard.domain.repository.UserRepository;
+
+import java.util.UUID;
+
+@Service
 public class CreateUserUseCase {
+
+    private final UserRepository repository;
+
+    public CreateUserUseCase(
+            UserRepository repository
+    ) {
+        this.repository = repository;
+    }
+
+    public User execute(CreateUserDTO dto) {
+
+        User user = new User(
+                UUID.randomUUID().toString(),
+                dto.name(),
+                dto.email(),
+                dto.password()
+        );
+
+        return repository.save(user);
+    }
 }
